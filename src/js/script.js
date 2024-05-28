25a364b3deecbb5b968bcc54c020ad6f70e6956e
orynbek <orikentos2006@gmail.com>
2024-05-28 17:27:55 +0500

    script

---
+++ src/js/script.js
@@ -0,0 +1,89 @@
+// alert(1);
+// переменные:
+// var
+// let
+// const
+var name = 'orynbek';
+let number = 10;
+const pi = 3.14;
+
+number = 15;
+
+let leftBorderWidth = 200;
+
+// типы данных:
+// number
+// string '' "" ``
+// true/false логические
+// null
+// undefined
+// {} объект
+// let obj{
+//   name: 'appple',
+//   color: 'green',
+//   weight: 200
+// }
+
+// console.log('hello');
+
+// console.log(number);
+
+// let answer = confirm('вам есть 18?')
+// console.log(answer);
+
+// let answer = prompt('вам есть 18?', "");
+// console.log(answer);
+
+// операторы:
+// console.log(10-5)
+// console.log(10/5)
+// console.log(10*5)
+// console.log(4-'hjkj')
+// console.log(4+'hjkj')
+
+// Логические операторы
+// let = isChecked = true,
+//       isClosed = false;
+// console.log(isChecked && isClosed);
+// console.log(isChecked||isClosed);
+
+// условные операторы
+// if (2*4==8){
+//   console.log('верно')
+// }else{
+//   console.log('ошибка')
+// }
+
+// let answer = confirm('вам есть 18?')
+// if(answer){
+//   console.log('проходите')
+// }else{
+//   console.log('уходите')
+// }
+
+// const num = 50;
+// if (num<49) {
+//   console.log('Неправильно')
+// } else if (num > 100) {
+//   console.log('много')
+// } else{
+//   console.log('правильно')
+// }
+
+// циклы
+// for (let i = 1; i < 8; i++){
+//   console.log(i)
+// }
+
+// функций
+// function  logging() {
+//   console.log(1234)
+// }
+// logging();
+
+// function  logging(a, b) {
+//   console.log(a-b)
+//   console.log(a+b)
+// }
+// logging(5, 6);
+// logging(8, 4);
