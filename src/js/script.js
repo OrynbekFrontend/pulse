15680d47e4a57dca55958185ca5d49ba268c6f77
orynbek <orikentos2006@gmail.com>
2024-06-01 15:40:47 +0500

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,89 +1,59 @@
-// alert(1);
-// переменные:
-// var
-// let
-// const
-var name = 'orynbek';
-let number = 10;
-const pi = 3.14;
+// $(document).ready(function(){
+//   $('.carousel__inner').slick({
+//     infinite: true,
+//     speed: 500,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="src/img/left.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="src/img/right.svg"></button>',
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           arrows:false,
+//           dots: true
+//       }
+//     }
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         infinite: true,
+//         dots: false
+//       }
+//     },
+//     {
+//       breakpoint: 992,
+//       settings: {
+//         arrows:false,
+//         dots:true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//     ]
+//   });
+// });
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  nav: false,
+  controls: false
+});
 
-number = 15;
-
-let leftBorderWidth = 200;
-
-// типы данных:
-// number
-// string '' "" ``
-// true/false логические
-// null
-// undefined
-// {} объект
-// let obj{
-//   name: 'appple',
-//   color: 'green',
-//   weight: 200
-// }
-
-// console.log('hello');
-
-// console.log(number);
-
-// let answer = confirm('вам есть 18?')
-// console.log(answer);
-
-// let answer = prompt('вам есть 18?', "");
-// console.log(answer);
-
-// операторы:
-// console.log(10-5)
-// console.log(10/5)
-// console.log(10*5)
-// console.log(4-'hjkj')
-// console.log(4+'hjkj')
-
-// Логические операторы
-// let = isChecked = true,
-//       isClosed = false;
-// console.log(isChecked && isClosed);
-// console.log(isChecked||isClosed);
-
-// условные операторы
-// if (2*4==8){
-//   console.log('верно')
-// }else{
-//   console.log('ошибка')
-// }
-
-// let answer = confirm('вам есть 18?')
-// if(answer){
-//   console.log('проходите')
-// }else{
-//   console.log('уходите')
-// }
-
-// const num = 50;
-// if (num<49) {
-//   console.log('Неправильно')
-// } else if (num > 100) {
-//   console.log('много')
-// } else{
-//   console.log('правильно')
-// }
-
-// циклы
-// for (let i = 1; i < 8; i++){
-//   console.log(i)
-// }
-
-// функций
-// function  logging() {
-//   console.log(1234)
-// }
-// logging();
-
-// function  logging(a, b) {
-//   console.log(a-b)
-//   console.log(a+b)
-// }
-// logging(5, 6);
-// logging(8, 4);
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
