d83c0e301f119cff509b25a1c4b44da0828a7d0c
orynbek <orikentos2006@gmail.com>
2024-06-04 01:08:11 +0500

    slider slick

--- src/js/script.js
+++ src/js/script.js
@@ -1,59 +1,37 @@
-// $(document).ready(function(){
-//   $('.carousel__inner').slick({
-//     infinite: true,
-//     speed: 500,
-//     prevArrow: '<button type="button" class="slick-prev"><img src="src/img/left.svg"></button>',
-//     nextArrow: '<button type="button" class="slick-next"><img src="src/img/right.svg"></button>',
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     responsive: [
-//       {
-//         breakpoint: 992,
-//         settings: {
-//           arrows:false,
-//           dots: true
-//       }
-//     }
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: false
-//       }
-//     },
-//     {
-//       breakpoint: 992,
-//       settings: {
-//         arrows:false,
-//         dots:true,
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
-//     ]
-//   });
-// });
-const slider = tns({
-  container: '.carousel__inner',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  nav: false,
-  controls: false
-});
-
-document.querySelector('.prev').addEventListener('click', function () {
-  slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function () {
-  slider.goTo('next');
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    infinite:true,
+    speed:300,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="src/img/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="src/img/right.svg"></button>',
+    responsive:[
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
