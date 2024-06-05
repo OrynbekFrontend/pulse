9a025b9b0e0c901b90169a71b397f0f7f45e8e73
orynbek <orikentos2006@gmail.com>
2024-06-06 01:15:57 +0500

    src

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     infinite:true,
     speed:300,
     dots: true,
+    focusOnSelect: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="src/img/left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="src/img/right.svg"></button>',
     responsive:[
@@ -34,4 +35,25 @@ $(document).ready(function(){
       }
     ]
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_activ)', function() {
+    $(this)
+      .addClass('catalog__tab_activ').siblings().removeClass('catalog__tab_activ')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  $('.catalog__link').each(function(i) {
+    $(this).on('click', function(e){
+      e.preventDefault();
+      $('.catalog__card_wrapper_content').eq(i).toggleClass('catalog__card_wrapper_content_active');
+      $('.catalog__card_wrapper_list').eq(i).toggleClass('catalog__card_wrapper_list_active');
+    })
+  })
+  $('.catalog__card_wrapper_back').each(function(i) {
+    $(this).on('click', function(e){
+      e.preventDefault();
+      $('.catalog__card_wrapper_content').eq(i).toggleClass('catalog__card_wrapper_content_active');
+      $('.catalog__card_wrapper_list').eq(i).toggleClass('catalog__card_wrapper_list_active');
+    })
+  })
 });
